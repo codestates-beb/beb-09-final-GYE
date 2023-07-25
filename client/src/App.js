25378d71ca95e179c429a87735787add8fc149f2
san <stcr96@gmail.com>
2023-07-25 17:41:48 +0900

    Fix nav css

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
 import Nav from './components/nav/Nav';
+
+// import Home from './pages/Home';
+// import Community from './pages/Community';
+// import Swap from './pages/Swap';
+
+function Layout() {
+  const location = useLocation();
+  const [page, setPage] = useState('home');
+
+  useEffect(() => {
+    const path = location.pathname.split('/')[1];
+    setPage(path ? path : 'home');
+  }, [location]);
+
+  return (
+    <div>
+      <Nav path={page} />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/swap" element={<Swap />} />
+      </Routes> */}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-      <Nav />
-      {/* 다른 컴포넌트들 추가 */}
-    </div>
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
   );
 }
 
