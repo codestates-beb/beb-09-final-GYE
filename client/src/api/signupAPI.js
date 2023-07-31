2d19bfa08b7ba27ec135da361deb75b07c568a9f
sungpan44 <sungpan44@gmail.com>
2023-08-01 00:20:21 +0900

    Clent/singupAPI

--- client/src/api/signupAPI.js
+++ client/src/api/signupAPI.js
@@ -1,16 +1,26 @@
 import axios from 'axios';
 
-export async function signupAPI(email, password, nickname, callback) {
-  try {
-    const formData = new FormData();
-    formData.append('email', email);
-    formData.append('password', password);
-    formData.append('nickname', nickname);
+export async function signupAPI(email, password, nickname) {
+  // try {
+  //   const formData = new FormData();
+  //   formData.append('email', email);
+  //   formData.append('password', password);
+  //   formData.append('nickname', nickname);
 
-    const response = await axios.post('http://localhost:5000/auth/signup', formData);
-    callback(null, response.data);
-    console.log(response.data);
-  } catch (error) {
-    callback(error, null);
+  //   const response = await axios.post('http://localhost:5000/auth/signup', formData);
+  //   callback(null, response.data);
+  //   console.log(response.data);
+  // } catch (error) {
+  //   callback(error, null);
+  // }
+  try {
+    const result = await axios({
+      url: "http://localhost:5000/user/signup",
+      method: "post",
+      data: { nickname, email, password },
+    });
+    return result.data;
+  } catch (err) {
+    return err.response.data;
   }
 }
--- server/server.js
+++ server/server.js
@@ -6,17 +6,16 @@ require("dotenv").config();
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 const port = parseInt(process.env.PORT, 10);
-
 const cors = require("cors");
 require("dotenv").config();
 
-const authRouter = require("./routes/users");
+const usersRouter = require("./routes/users");
 const groupRouter = require("./routes/creategye");
 const groupList = require("./routes/gyelist")
 
 app.use(cors());
 app.use(express.json());
-app.use("/user", authRouter);
+app.use("/user", usersRouter);
 app.use("/group", groupRouter);
 app.use("/grouplist", groupList)
 
