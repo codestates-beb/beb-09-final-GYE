52aec0d26e40efaed20751b1f774d23b0a486a82
codex1928 <ldus1928@gmail.com>
2023-08-07 08:51:26 +0900

    App.js 업데이트

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
-import Main from './pages/Main';
-// import Community from './pages/Community';
+import Main from "./pages/Main";
+import Community from "./pages/Community";
 // import Swap from './pages/Swap';
 
 function Layout() {
   const location = useLocation();
-  const [page, setPage] = useState('main');
+  const [page, setPage] = useState("main");
 
   useEffect(() => {
-    const path = location.pathname.split('/')[1];
-    setPage(path ? path : 'main');
+    const path = location.pathname.split("/")[1];
+    setPage(path ? path : "main");
   }, [location]);
 
   return (
     <div>
       <Routes>
         <Route path="/" element={<Main />} />
-        {/* <Route path="/community" element={<Community />} />
-        <Route path="/swap" element={<Swap />} /> */}
+        <Route path="/community" element={<Community />} />
+        {/* <Route path="/swap" element={<Swap />} /> */}
       </Routes>
     </div>
   );
