9dcd7cf05a1af3931bbc4955701a391e2ea469f8
codex1928 <ldus1928@gmail.com>
2023-08-07 08:41:33 +0900

    community 1차개발 완성본

---
+++ client/src/pages/login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 로그인 처리 로직
+    // 서버에 API 요청을 보내거나 등등...
+    console.log(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
